0db137141a0965c038fe3515e3151082f8c4a959
sandroroux <aroux@edx.org>
2018-04-23 10:09:23 -0400

    Debug: remove the mixin's influence

--- openedx/core/djangolib/model_mixins.py
+++ openedx/core/djangolib/model_mixins.py
@@ -20,24 +20,17 @@ class DeletableByUserValue(object):
     associated with some specified user.
     """
 
-    @classmethod
-    def delete_by_user_value(cls, value, field):
-        """
-        Deletes instances of this model where ``field`` equals ``value``.
+    # @classmethod
+    # def delete_by_user_value(cls, value, field):
+    #     """
+    #     Deletes instances of this model where ``field`` equals ``value``.
 
-        e.g.
-            ``delete_by_user_value(value='learner@example.com', field='email')``
+    #     e.g.
+    #         ``delete_by_user_value(value='learner@example.com', field='email')``
 
-        Returns True if any instances were deleted.
-        Returns False otherwise.
-        """
-        filter_kwargs = {field: value}
-        print '-' * 20 + 'DEBUG' + '-' * 20
-        print 'filter_kwargs: {}'.format(filter_kwargs)
-        print 'cls: {}'.format(cls)
-        print 'cls.objects: {}'.format(cls.objects)
-        print 'cls.objects.filter(**filter_kwargs): {}'.format(cls.objects.filter(**filter_kwargs))
-        print 'cls.objects.filter(**filter_kwards).delete(): {}'.format(cls.objects.filter(**filter_kwargs).delete())
-        print '-' * 20 + 'DEBUG' + '-' * 20
-        num_deleted_records, _ = cls.objects.filter(**filter_kwargs).delete()
-        return num_deleted_records > 0
+    #     Returns True if any instances were deleted.
+    #     Returns False otherwise.
+    #     """
+    #     filter_kwargs = {field: value}
+    #     num_deleted_records, _ = cls.objects.filter(**filter_kwargs).delete()
+    #     return num_deleted_records > 0
